4c70d90da8da13228eada64e7ea0089f4ee078a2
Arun S. Negi <arunsnegi@protonmail.com>
2020-05-11 17:23:53 +0530

    Feat: Pause timer on hover.

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -6,4 +6,31 @@ const removeElement = (el) => {
   }
 };
 
-export {removeElement}
+// custom timer with pause, resume ability
+// thanks to: https://stackoverflow.com/a/3969760
+class Timer {
+  constructor(callback, delay) {
+    this.startTime = Date.now();
+    this.callback = callback;
+    this.delay = delay;
+
+    this.timer = setTimeout(callback, delay);
+  }
+
+  pause() {
+    this.stop();
+    this.delay -= Date.now() - this.startTime;
+  }
+
+  resume() {
+    this.stop();
+    this.startTime = Date.now();
+    this.timer = setTimeout(this.callback, this.delay);
+  }
+
+  stop() {
+    clearTimeout(this.timer);
+  }
+}
+
+export {removeElement, Timer}
